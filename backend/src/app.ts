331b01131b9e5cc81f55a4b3d18cf9e4ce482dc3
robertshum <robertshum@gmail.com>
2024-03-22 21:54:39 -0700

    add react query and fetch test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+
+// Enable CORS for all routes
+// TODO might need to update for security reasons
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to my Express application');
 });
 
 app.get('/api/helloworld', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World' });
+  res.json({ id: 1, message: 'Hello Paint World' });
 });
 
 const PORT = process.env.PORT || 3000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import { useQuery } from 'react-query';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // Test
+  const { data, isLoading, error } = useQuery('helloworld', async () => {
+    const response = await fetch('http://localhost:3000/api/helloworld');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  });
+
+  // state management from query
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <>
+      <div>
+        <h1>Hello from Backend</h1>
+        <p>ID: {data.anotherId}</p>
+        <p>Message: {data.message}</p>
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +50,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
