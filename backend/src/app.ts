d75587b98ba8caefba43372e320b5c2ad3059ed6
robertshum <robertshum@gmail.com>
2024-03-23 21:12:14 -0700

    test netlify api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,26 +1,27 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Router } from 'express';
 import cors from 'cors';
-
+import serverless from "serverless-http";
 import fs from 'fs';
 import path from 'path';
 const dbPath = path.resolve(__dirname, '../db/mock/db.json');
 
 const app = express();
+const router = Router();
 
 // Enable CORS for all routes
 // TODO might need to update for security reasons
-app.use(cors());
+router.use(cors());
 
 // Middleware to parse JSON request bodies
-app.use(express.json());
+router.use(express.json());
 
-app.get('/api/paints', (req: Request, res: Response) => {
+router.get('/paints', (req: Request, res: Response) => {
   const data = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
   res.json(data.paint);
 });
 
 // Update Paint stock
-app.post('/api/paints', (req: Request, res: Response) => {
+router.post('/paints', (req: Request, res: Response) => {
   const data = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
   const newData = req.body;
   console.log("newData", newData);
@@ -33,7 +34,7 @@ app.post('/api/paints', (req: Request, res: Response) => {
   res.json(200);
 });
 
-app.get('/api/paintremoval', (req: Request, res: Response) => {
+router.get('/paintremoval', (req: Request, res: Response) => {
   const data = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
   const users = data.users;
 
@@ -43,11 +44,11 @@ app.get('/api/paintremoval', (req: Request, res: Response) => {
   res.json(data.users);
 });
 
-app.get('/', (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
   res.send('Welcome to my Express application');
 });
 
-app.get('/api/helloworld', (req: Request, res: Response) => {
+router.get('/helloworld', (req: Request, res: Response) => {
   res.json({ id: 1, message: 'Hello Paint World' });
 });
 
@@ -55,3 +56,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use("/api/", router);
+
+export const handler = serverless(app);
